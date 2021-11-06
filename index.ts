d998134a0ab596ed099f071b649580ba5bb609e6
YoniB1995 <yonatansamfisher@gmail.com>
2021-11-06 17:18:32 +0200

    ts types basics

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
 let num: number = 5;
+let word: string = "Yoni";
+let isLearning: boolean = true;
+let yoniB: any = "2rafag";
+
+
+let ids: number[] = [1,2,3,4];
 
 // id = "5"; // error - type number can't convert to string.
